77fd1bc44f055aa7666185a281d7dd8dbb03402e
M A Murad <72880355+mozaddedalfeshani@users.noreply.github.com>
2025-01-21 05:19:38 +0600

    feat: update index title to 'Wander Wise'; modify asset cache and change HOST to production URL

--- src/constant/HOST.jsx
+++ src/constant/HOST.jsx
@@ -1,3 +1,3 @@
-// const HOST = "https://wanderwise-beta.vercel.app";
-const HOST = "http://localhost:9000";
+const HOST = "https://wanderwise-beta.vercel.app";
+// const HOST = "http://localhost:9000";
 export default HOST;
